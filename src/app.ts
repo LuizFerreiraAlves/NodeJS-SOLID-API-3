98f205945e080eb56be179e2bd621e9b47e2589e
Luiz Ferreira Alves <luiz.ferreira.alves30@gmail.com>
2026-06-24 13:05:17 -0300

    Staged changes for prsiam and ap

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
 import fastify from "fastify";
+import { prisma } from "@/lib/prisma";
 
 export const app = fastify()
+
+// prisma.user.create({
+//     data: {
+//         name: 'Luiz Ferreira Alves',
+//         email: 'luiz.ferreira.alves30@gmail.com'
+//     }
+// })
