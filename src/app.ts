93e7c3cd9cc11024ec816f09a903ed049e51629f
Luiz Ferreira Alves <luiz.ferreira.alves30@gmail.com>
2026-07-01 00:08:55 -0300

    Route to create a user

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,25 @@
 import fastify from "fastify";
 import { prisma } from "@/lib/prisma";
+import { z } from "zod";
 
 export const app = fastify()
 
-// prisma.user.create({
-//     data: {
-//         name: 'Luiz Ferreira Alves',
-//         email: 'luiz.ferreira.alves30@gmail.com'
-//     }
-// })
+app.post('/users', async (request, response) => {
+    const registerBodySchema = z.object({
+        name: z.string(),
+        email: z.email(),
+        password: z.string().min(6),
+    })
+
+    const { name, email, password } = registerBodySchema.parse(request.body)
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash: password,
+        }
+    })
+
+    return response.status(201).send()
+})
