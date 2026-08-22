513fb0a94762ce374370a9ad17d7d12ff2d862e4
Luiz Ferreira Alves <luiz.ferreira.alves30@gmail.com>
2026-08-22 13:40:17 -0300

    Understanding JWT principles

---
+++ src/http/controllers/profile.ts
@@ -0,0 +1,9 @@
+import { FastifyRequest, FastifyReply } from "fastify"
+
+export async function profile(
+    request: FastifyRequest, 
+    response: FastifyReply
+) {
+
+    return response.status(200).send()
+}
