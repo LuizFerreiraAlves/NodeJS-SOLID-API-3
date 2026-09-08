b54b3078b4882fff1c2d6137b781c0550c29195a
Luiz Ferreira Alves <luiz.ferreira.alves30@gmail.com>
2026-09-07 22:19:13 -0300

    Creating the test environment

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,43 @@
+import "dotenv/config"
+import { randomUUID } from "node:crypto"
+import type { Environment } from "vitest/environments"
+import { execSync } from "node:child_process"
+import { prisma } from "@/lib/prisma"
+
+function generateDatabaseUrl(schema: string) {
+    if (!process.env.DATABASE_URL) {
+        throw new Error('Please provide a DATABASE_URL environment variable to run the tests.')
+    }
+
+    const url = new URL(process.env.DATABASE_URL)
+    url.searchParams.set('schema', schema)
+
+    return url.toString()
+}
+
+export default <Environment>{
+    name: 'prisma',
+    transformMode: 'ssr',
+    async setup() {
+        // Create the test database
+        const schema = randomUUID()
+        const databaseUrl = generateDatabaseUrl(schema)
+
+        console.log(databaseUrl)
+
+        process.env.DATABASE_URL = databaseUrl
+
+        execSync('npx prisma db push')
+
+        return {
+            async teardown() {
+                // Delete the test database
+                await prisma.$executeRawUnsafe(
+                    `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+                )
+
+                await prisma.$disconnect()
+            },
+        }
+    },
+}
