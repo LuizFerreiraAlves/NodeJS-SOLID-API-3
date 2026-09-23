f6a5ce47e15382030a9a35a1fcd29a6194da3979
Luiz Ferreira Alves <luiz.ferreira.alves30@gmail.com>
2026-09-23 20:47:50 -0300

    Starting refresh token implementation

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -31,9 +31,27 @@ export async function authenticate(
             },
         )
 
-        return response.status(200).send({
-            token,
-        })
+        const refreshToken = await response.jwtSign(
+            {}, 
+            {
+                sign: {
+                    sub: user.id,
+                    expiresIn: '7d',
+                },
+            },
+        )
+
+        return response
+            .setCookie('refreshToken', refreshToken, {
+                path: '/',
+                secure: true,
+                sameSite: true,
+                httpOnly: true,
+            })
+            .status(200)
+            .send({
+                token,
+            })
 
     } catch (err) {
         if (err instanceof InvalidCredentialsError) {
